3a30ad95af8fb7caf027ac787a97610f8f9df91c
Sushruth Chandrasekar <sushruth31@gmail.com>
2022-09-10 22:05:28 -0400

    updates

--- src/pokemon.rs
+++ src/pokemon.rs
@@ -1,5 +1,6 @@
 use gloo::timers::callback::Timeout;
 use gloo_console::{console, log};
+use rand::prelude::*;
 use reqwest;
 use serde_json::Value;
 use wasm_bindgen_futures;
@@ -8,7 +9,7 @@ use web_sys::console;
 use yew::prelude::*;
 use yew_router::{navigator, prelude::*, switch::_SwitchProps::render};
 
-#[derive(Debug, Properties, PartialEq)]
+#[derive(Debug, Properties, PartialEq, Clone)]
 pub struct PokemonData {
     name: String,
     img_src: String,
@@ -17,11 +18,18 @@ pub struct PokemonData {
 #[function_component(Pokemon)]
 pub fn pokemon() -> Html {
     let pokemon: UseStateHandle<Option<PokemonData>> = use_state(|| None);
-    console::log_1(&format!("{:?}", pokemon).into());
+    let loading: UseStateHandle<bool> = use_state(|| false);
+    let loading_copy = loading.clone();
+    let pokemon_copy = pokemon.clone();
+    //console::log_1(&format!("{:?}", pokemon).into());
     let onclick = Callback::from(move |e: MouseEvent| {
         let pokemon_state = pokemon.clone();
+        let loading = loading.clone();
         wasm_bindgen_futures::spawn_local(async move {
-            let url = "https://pokeapi.co/api/v2/pokemon/22";
+            loading.set(true);
+            let mut rng = rand::thread_rng();
+            let poke_id: u32 = rng.gen_range(1..100);
+            let url = format!("https://pokeapi.co/api/v2/pokemon/{}", poke_id);
             match reqwest::get(url).await {
                 Ok(response) => {
                     if response.status() == reqwest::StatusCode::OK {
@@ -29,12 +37,11 @@ pub fn pokemon() -> Html {
                         let data: Value = serde_json::from_str(&text).unwrap();
                         let name = data["name"].as_str().unwrap();
                         let img_src = data["sprites"]["front_default"].as_str().unwrap();
-                        let pokemon = PokemonData {
+                        pokemon_state.set(Some(PokemonData {
                             name: name.into(),
                             img_src: img_src.into(),
-                        };
-                        pokemon_state.set(Some(pokemon));
-                        log!("fetched!");
+                        }));
+                        loading.set(false);
                     }
                 }
 
@@ -46,7 +53,31 @@ pub fn pokemon() -> Html {
     });
     html! {
         <>
+            <h4>{"Your Pokemon"}</h4>
+            if *loading_copy {
+                <div>{"Fetching..."}</div>
+            }
+            <PokemonView pokemon={(*pokemon_copy).clone()}/>
             <button {onclick}>{"Fetch Pokemon"}</button>
             </>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct PokemonViewProps {
+    pokemon: Option<PokemonData>,
+}
+
+#[function_component(PokemonView)]
+pub fn pokemon_view(props: &PokemonViewProps) -> Html {
+    if let Some(pokemon) = &props.pokemon {
+        return html! {
+            <>
+                <div>{"Pokemon Details"}</div>
+                <div>{"Name: "} {pokemon.name.clone()}</div>
+                </>
+        };
+    } else {
+        return html! {};
     }
 }
